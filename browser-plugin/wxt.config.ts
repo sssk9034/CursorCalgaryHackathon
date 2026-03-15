67f52c44801258e3d8e417b9412bb1a873e4af90
Angelo Aldecoa <angelokn14@gmail.com>
2026-03-14 19:21:51 -0600

    Rename plugin to Everest

--- browser-plugin/wxt.config.ts
+++ browser-plugin/wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    name: "Deep Focus",
+    name: "Everest",
     permissions: ["tabs", "tabGroups", "storage"],
     host_permissions: ["http://127.0.0.1:3456/*"],
   },
